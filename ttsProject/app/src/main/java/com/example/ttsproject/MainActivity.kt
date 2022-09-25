a24fcd6a5d4b7ffb0d6f1ce5a9905a59a2561e75
HanYong <toy9910@naver.com>
2022-09-25 12:01:31 +0900

    mediapipe 적용

--- ttsProject/app/src/main/java/com/example/ttsproject/MainActivity.kt
+++ ttsProject/app/src/main/java/com/example/ttsproject/MainActivity.kt
@@ -2,23 +2,33 @@ package com.example.ttsproject
 
 import android.Manifest
 import android.content.Intent
-import android.icu.lang.UCharacter.GraphemeClusterBreak.V
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.RecognitionListener
 import android.speech.RecognizerIntent
 import android.speech.SpeechRecognizer
 import android.speech.tts.TextToSpeech
+import android.util.Log
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.ttsproject.databinding.ActivityMainBinding
+import com.google.mediapipe.solutioncore.CameraInput
+import com.google.mediapipe.solutioncore.SolutionGlSurfaceView
+import com.google.mediapipe.solutions.hands.HandLandmark
+import com.google.mediapipe.solutions.hands.Hands
+import com.google.mediapipe.solutions.hands.HandsOptions
+import com.google.mediapipe.solutions.hands.HandsResult
 import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.TedPermission
-import java.security.Permission
 import java.util.*
 
 class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+    private val TAG = "MainActivity"
     private lateinit var binding : ActivityMainBinding
     private lateinit var tts : TextToSpeech
+    private lateinit var hands : Hands
+    private lateinit var cameraInput: CameraInput
+    private lateinit var glSurfaceView: SolutionGlSurfaceView<HandsResult>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,7 +48,8 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
         TedPermission.with(this)
             .setPermissionListener(permissionListener)
-            .setPermissions(Manifest.permission.INTERNET, Manifest.permission.RECORD_AUDIO)
+            .setPermissions(Manifest.permission.INTERNET, Manifest.permission.RECORD_AUDIO, Manifest.permission.CAMERA,
+            Manifest.permission.ACCESS_NETWORK_STATE)
             .check()
 
 
@@ -49,12 +60,79 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         binding.btnRecord.setOnClickListener {
             startSTT()
         }
+
+        setupStreamingModePipeline()
     }
 
     override fun onInit(p0: Int) {
         if(p0 == TextToSpeech.SUCCESS) {
             val result = tts.setLanguage(Locale.KOREAN)
             speakOut()
+        }
+    }
+
+    private fun startCamera() {
+        cameraInput.start(
+            this,
+            hands.glContext,
+            CameraInput.CameraFacing.FRONT,
+            glSurfaceView.width,
+            glSurfaceView.height
+        )
+    }
+
+    override fun onResume() {
+        super.onResume()
+        cameraInput = CameraInput(this)
+        cameraInput.setNewFrameListener { hands.send(it) }
+        glSurfaceView.post { this.startCamera() }
+        glSurfaceView.visibility = View.VISIBLE
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+//        glSurfaceView.visibility = View.GONE
+//        cameraInput.close()
+    }
+
+    private fun setupStreamingModePipeline() {
+        hands = Hands(
+            this,
+            HandsOptions.builder()
+                .setStaticImageMode(false)
+                .setMaxNumHands(1)
+                .setRunOnGpu(true)
+                .build()
+        )
+        hands.setErrorListener { message, e -> Log.e(TAG, "MediaPipe Hands error: $message") }
+
+        cameraInput = CameraInput(this)
+        cameraInput.setNewFrameListener { hands.send(it) }
+
+        glSurfaceView = SolutionGlSurfaceView(this, hands.glContext, hands.glMajorVersion)
+        glSurfaceView.setSolutionResultRenderer(HandsResultGlRenderer())
+        glSurfaceView.setRenderInputImage(true)
+        hands.setResultListener {
+            logWristLandmark(it, false)
+            glSurfaceView.setRenderData(it)
+            glSurfaceView.requestRender()
+        }
+
+        glSurfaceView.post(this::startCamera)
+
+        binding.previewDisplayLayout.apply {
+            removeAllViewsInLayout()
+            addView(glSurfaceView)
+            glSurfaceView.visibility = View.VISIBLE
+            requestLayout()
+        }
+    }
+
+    private fun stopCurrentPipeline() {
+        if(cameraInput != null) {
+            cameraInput.setNewFrameListener(null)
+            cameraInput.close()
         }
     }
 
@@ -106,5 +184,44 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
             Toast.makeText(this@MainActivity, "음성인식 종료", Toast.LENGTH_SHORT).show()
             binding.tvResult.text = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)!![0]
         }
+    }
+
+
+
+    private fun logWristLandmark(result: HandsResult, showPixelValues: Boolean) {
+        if (result.multiHandLandmarks().isEmpty()) {
+            return
+        }
+        val wristLandmark = result.multiHandLandmarks()[0].landmarkList[HandLandmark.WRIST]
+        // For Bitmaps, show the pixel values. For texture inputs, show the normalized coordinates.
+        if (showPixelValues) {
+            val width = result.inputBitmap().width
+            val height = result.inputBitmap().height
+            Log.i(
+                TAG, String.format(
+                    "MediaPipe Hand wrist coordinates (pixel values): x=%f, y=%f",
+                    wristLandmark.x * width, wristLandmark.y * height
+                )
+            )
+        } else {
+//            Log.i(
+//                TAG, String.format(
+//                    "MediaPipe Hand wrist normalized coordinates (value range: [0, 1]): x=%f, y=%f",
+//                    wristLandmark.x, wristLandmark.y
+//                )
+//            )
+        }
+        if (result.multiHandWorldLandmarks().isEmpty()) {
+            return
+        }
+        val wristWorldLandmark =
+            result.multiHandWorldLandmarks()[0].landmarkList[HandLandmark.WRIST]
+        Log.i(
+            TAG, String.format(
+                "MediaPipe Hand wrist world coordinates (in meters with the origin at the hand's"
+                        + " approximate geometric center): x=%f , y=%f , z=%f ",
+                wristWorldLandmark.x, wristWorldLandmark.y, wristWorldLandmark.z
+            )
+        )
     }
 }
